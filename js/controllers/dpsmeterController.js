e2ec5494fec961c7977fa004f4f8043e888e7fe8
Enilia <florian.olivierandre@gmail.com>
2015-09-20 17:07:59 +0200

    dpsmeter data update skeleton

--- js/controllers/dpsmeterController.js
+++ js/controllers/dpsmeterController.js
@@ -1,5 +1,11 @@
 angular.module('enilia.overlay.controllers.dpsmeterController', [])
 
-	.controller('dpsmeterController', ['$scope', function($scope) {
+	.controller('dpsmeterController', ['$scope', '$interval', '$document', function($scope, $interval, $document) {
+
+		$document.on('onOverlayDataUpdate', dataUpdate);
+
+		function dataUpdate() {
+			$scope.$apply();
+		}
 
 	}])
