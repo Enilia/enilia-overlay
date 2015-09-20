e8748e34f50eae05245b8f43f04a7e30b91d8ed1
Enilia <florian.olivierandre@gmail.com>
2015-09-20 07:06:43 +0200

    renamed test controller and removed test crap
    test controller is now known as stateWatcherController

--- js/app.js
+++ js/app.js
@@ -1,23 +1,12 @@
 
 angular.module('enilia.overlay', ['enilia.overlay.tpls'])
 
-	.controller('test', ['$scope','$document', '$location', function($scope, $document, $location) {
+	.controller('stateWatcherController', ['$scope','$document', function($scope, $document) {
 
-		var lastTime = Date.now();
-
-		$scope.location = $location.url();
-		$scope.data = "no data";
 		$scope.state = { isLocked: true };
 
-		$document.on('onOverlayDataUpdate', dataUpdate);
+		// $document.on('onOverlayDataUpdate', dataUpdate);
 		$document.on('onOverlayStateUpdate', stateUpdate);
-
-	    function dataUpdate(e) {
-
-	        $scope.data = Date.now() - lastTime;
-	        $scope.$apply();
-	        lastTime = Date.now();
-	    }
 
 	    function stateUpdate(e) {
 
