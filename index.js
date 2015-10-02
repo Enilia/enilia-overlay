70112eedc4495cf36afe712cb5480d20320f2cec
Enilia <florian.olivierandre@gmail.com>
2015-10-02 08:43:59 +0200

    Promisified build process

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ var program = require('commander')
   , util = require('util')
   , watch = require('node-watch')
   , package = require('./package.json')
+  , Promise = require("bluebird")
+  , build = Promise.resolve()
   ;
 
 program
@@ -16,30 +18,47 @@ program
 
 
 function log() {
-	console.log.apply(console, ["[%s]", new Date().toTimeString().split(' ')[0]].concat(Array.from(arguments)))
+	var now = new Date().toTimeString().split(' ')[0]
+	  , args = util.format.apply(util, arguments)
+	  ;
+	console.log.apply(console, ["[%s]", now, args])
+}
+
+function thenlog() {
+	var args = arguments;
+	return function() {
+		log.apply(null, args);
+	}
 }
 
 function main() {
 	delete require.cache[path.resolve('./package.json')]
 	package = require('./package.json')
 
+	var now = Date.now();
+
 	if(program.buildlocal) {
-		log("build local app");
-		var local = require('./scripts/build.js')(package.config.build);
-		if(program.release) local.then(release)
+		build = build.then(thenlog("building local app"))
+					 .return(package.config.build).then(require('./scripts/build.js'));
+		if(program.release) build = build.then(release)
 	} else if(program.release) {
-		release();
+		build = build.then(release);
 	}
 	if(program.buildparse) {
-		log("build parse app");
-		require('./scripts/build.js')(package.config.parse);
+		build = build.then(thenlog("building parse app"))
+					 .return(package.config.parse).then(require('./scripts/build.js'));
 	}
+	build = build.then(function() {
+		log("build time: %ss", (Date.now() - now) / 1000);
+	});
 }
 
 function release() {
-	require('./scripts/buildRelease.js')(function(pointer, outName) {
-		log('%s bytes written in %s', pointer, outName);
-	})
+	var now = Date.now();
+	return Promise.promisify(require('./scripts/buildRelease.js'))()
+		.spread(function(pointer, outName) {
+			log('%s bytes written in %s (%ss)', pointer, outName, (Date.now() - now) / 1000);
+		})
 }
 
 if(program.watch) {
