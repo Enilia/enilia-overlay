4c116961bef97f4242e8aa44aad1ff3be531b45a
Enilia <florian.olivierandre@gmail.com>
2015-10-02 08:25:43 +0200

    converted to fs-extra-promise

--- scripts/getJobCss.js
+++ scripts/getJobCss.js
@@ -1,15 +1,22 @@
-var fs = require('promised-io/fs')
+var fs = require('fs-extra-promise')
   , util = require('util')
   , path = require('path')
   ;
 
 var line = ".%s .job { background-image:url('../images/glow/%s') }"
 
-fs.readdir('src/images/glow').then(function(files) {
+fs.readdirAsync('src/images/glow')
+.then(function(files) {
+	return files.sort(function(a, b) {
+	    return a.localeCompare(b);
+	})
+})
+.then(function(files) {
     return files.map(function(file) {
     	var name = path.basename(file.slice(0,1).toUpperCase() + file.slice(1), '.png').replace(/ /g, '-');
         return util.format(line, name, file);
     })
-}).then(function(r) {
-    return fs.writeFile('src/css/jobs.css', r.join('\n'), 'utf8')
+})
+.then(function(r) {
+    return fs.writeFileAsync('src/css/jobs.css', r.join('\n'), 'utf8')
 })
